197ec6fa24b2ad29ab9657e7b5daff82c7056e64
Hari haran <hariharan.uno@gmail.com>
2014-01-14 09:33:14 +0530

    Count number of Inversions

---
+++ countinversions/README.txt
@@ -0,0 +1,3 @@
+This file(IntegerArray.txt) contains all of the 100,000 integers between 1 and 100,000 (inclusive) in some order, with no integer repeated.
+
+Compute the number of inversions in the file given, where the ith row of the file indicates the ith entry of an array.
---
+++ countinversions/countInversions.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	var array []int64
+	// open input file
+	file, err := os.Open("IntegerArray.txt")
+	if err != nil {
+		panic(err)
+	}
+	// close file on exit and check for its returned error
+	// make a read buffer
+	r := bufio.NewReader(fi)
+	scanner := bufio.NewScanner(r)
+	num := 0
+	for scanner.Scan() {
+		num, err = strconv.Atoi(scanner.Text())
+		array = append(array, int64(num))
+	}
+	_, p := sortCount(array)
+	fmt.Printf("%v\n", p)
+
+}
+
+func sortCount(A []int64) ([]int64, int64) {
+	if len(A) == 1 {
+		return A, 0
+	} else {
+		B, x := sortCount(A[:len(A)/2])
+		C, y := sortCount(A[len(A)/2:])
+		D, z := countSplitInv(B, C)
+		return D, x + y + z
+	}
+	return A, 0
+}
+
+func countSplitInv(c []int64, d []int64) ([]int64, int64) {
+	b := make([]int64, (len(c) + len(d)))
+	i, j := 0, 0
+	var inv int64
+	for i < len(c) && j < len(d) {
+		if c[i] < d[j] {
+			b[i+j] = c[i]
+			i++
+		} else {
+			b[i+j] = d[j]
+			j++
+			inv = inv + int64((len(c) - i))
+		}
+	}
+	for i < len(c) {
+		b[i+j] = c[i]
+		i++
+	}
+	for j < len(d) {
+		b[i+j] = d[j]
+		j++
+	}
+	return b, inv
+}
